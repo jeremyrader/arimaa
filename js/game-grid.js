cbaa21d707a9d0d6aff76134273b78380f5de951
Jeremy Rader <jeremyr8r@gmail.com>
2016-02-15 20:51:18 -0700

    finish model for tracking game state

--- js/game-grid.js
+++ js/game-grid.js
@@ -1,16 +1,20 @@
 function showGameBoard() {
+    
     var gameGrid = document.getElementById('game-grid');
     gameGrid.innerHTML = generateTable(8,8);
 
     Game.init(8,8);
-    Game.evaluate('B8');
+    
+    drawPits();
+    drawPieces();
+    drawAvailableActions();
 
 }
 
 
 function generateTable(rows, columns) {
     
-    var table = '<table>';
+    var table = '<table id="game-squares">';
     
     //Generate new rows
     for (var i = rows; i > 0; i--) {
@@ -34,64 +38,64 @@ function generateColumn(columns, number) {
 
     //Iterate over columns to create cells
     for (var i = 1; i < columns + 1; i++) {
-        
-        var td = '<td>'; 
-
-        var location = String.fromCharCode(96 + i).toUpperCase() + number;
-
-        td = addId(td, location);
-
-         //Change the class of these 4 squares to create pits
-        if (location === 'C3' || location === 'F3' || location === 'C6' || location === 'F6') {
-            td = addClass(td, 'pit');
-        }
-
-        //Logic to be added later for determining which squares are available for the
-        //currently selected piece
-        //==========================================================================================
-        //For the current selected piece (white elephant) add borders to the squares that the piece may move to.
-        if (location === 'E5' || location === 'G5' || location === 'F6') {
-            td = addClass(td, 'move');
-        }
-
-        //For the current selected piece (white elephant) add borders to the squares that the piece may interact with
-        if (location === 'F4')
-        {
-            td = addClass(td, 'interact');
-        }
-        //==========================================================================================
-
-        column += td + '</td>';
+        column += '<td></td>';
     }
 
     return column;
 }
-
-
-
-function addClass(element, className) {
+        
+function drawPieces() {
     
-    var tagFragments;
-    
-    //If the element already has a class attribute then append the new class
-    //Otherwise, add the class attribute and the class
-    if (element.indexOf('class') > -1) {
-        tagFragments = element.split('class=\"');
-        element = tagFragments[0] + 'class=\"' + className + ' ' + tagFragments[1];
-    }
-    else {
-        tagFragments = element.split('>');
-        element = tagFragments[0] + ' class=\"' + className + '\">';
+    var gridTable = document.getElementById('game-squares');
+
+    for(var row = 0; row < Game.rows; row++) {
+
+        for(var column = 0; column < Game.columns;  column++) {
+            
+            var cell = gridTable.rows[row].cells[column];
+            var piece = Game.locations[row][column];
+            
+            if (piece !== null) {
+                cell.innerHTML = '<img src=\"../images/' + piece.loyalty + '-' + piece.rank + '.svg\" width=100% height=100%">';
+            }
+
+        }
+           
     }
 
-    return element;
 }
 
+function drawPits() {
+    
+    var gridTable = document.getElementById('game-squares');
+    
+    var cell1 = gridTable.rows[2].cells[2];
+    var cell2 = gridTable.rows[5].cells[5];
+    var cell3 = gridTable.rows[2].cells[5];
+    var cell4 = gridTable.rows[5].cells[2];
 
-function addId(element, id) {
+    cell1.className += 'pit';
+    cell2.className += 'pit';  
+    cell3.className += 'pit';  
+    cell4.className += 'pit';
+}
 
-    var tagFragments = element.split('>');
+function drawAvailableActions() {
+    
+    var selectedPiece = Game.selectedPiece;
 
-    return tagFragments[0] + ' id=\"' + id + '\">';
+    var gridTable = document.getElementById('game-squares');
+    var vacantSquares = Game.locations[selectedPiece[0]][selectedPiece[1]].vacantSquares;
+    var movablePieces = Game.locations[selectedPiece[0]][selectedPiece[1]].movablePieces;
 
+    movablePieces.forEach(function(piece) {
+        var cell = gridTable.rows[piece[0]].cells[piece[1]];
+        cell.className += 'interact';
+    });
+
+    vacantSquares.forEach(function(square) { 
+        var cell = gridTable.rows[square[0]].cells[square[1]];
+        cell.className += 'move';
+    });
+    
 }
--- js/model.js
+++ js/model.js
@@ -6,108 +6,233 @@ var Game = {
         this.columns = columns;
 
         for (var i = 1; i <= columns; i++) {
-
             for (var j = 1; j <= rows; j++) {
-                var location = String.fromCharCode(96 + i).toUpperCase() + j;
-                this.locations[location] = null;
+                this.locations = null;
+            }
+        }
+        
+        this.turn = 'b';
+        this.selectedPiece = [1,3];
+        
+        this.locations = [
+            [
+                { loyalty : 'w', rank : '6' },
+                { loyalty : 'b', rank : '6' },
+                null,
+                null,
+                null,
+                { loyalty : 'b', rank : '6' },
+                null,
+                null
+            ],
+            [
+                { loyalty : 'b', rank : '4' },
+                { loyalty : 'w', rank : '6' },
+                { loyalty : 'w', rank : '6' },
+                { loyalty : 'b', rank : '1' },
+                null,
+                { loyalty : 'b', rank : '6' },
+                null,
+                null
+            ],
+            [
+                null,
+                { loyalty : 'b', rank : '2' },
+                { loyalty : 'b', rank : '6' },
+                { loyalty : 'w', rank : '3' },
+                { loyalty : 'w', rank : '1' },
+                null,
+                null,
+                null
+            ], 
+            [
+                { loyalty : 'w', rank : '6' },
+                { loyalty : 'w', rank : '5' },
+                { loyalty : 'w', rank : '3' },
+                { loyalty : 'b', rank : '5' },
+                null,
+                { loyalty : 'b', rank : '6' },
+                null,
+                null
+            ],
+            [
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                { loyalty : 'w', rank : '2' },
+                null
+            ],
+            [
+                null,
+                null,
+                null,
+                null,
+                { loyalty : 'b', rank : '6' },
+                null,
+                null,
+                null
+            ],
+            [
+                { loyalty : 'w', rank : '6' },
+                null,
+                { loyalty : 'w', rank : '4' },
+                null,
+                null,
+                null,
+                { loyalty : 'b', rank : '5' },
+                { loyalty : 'b', rank : '6' }
+            ],
+            [
+                { loyalty : 'w', rank : '6' },
+                { loyalty : 'w', rank : '6' },
+                null,
+                null,
+                null,
+                null,
+                null,
+                { loyalty : 'b', rank : '6' }
+            ],
+        ];
+
+        for (var i = 0; i < rows; i++) {
+
+            for (var j = 0; j < columns; j++) {
+                this.evaluateSquare(i, j);
             }
 
         }
-
-        //Instantiate one piece for now for testing
-        this.locations['B8'] = { loyalty : 'w', rank : '5', weakerEnemies : [] };
-
-        this.turn = 'w';
-
+        
     },
 
-    evaluate : function(location) {
-
+    evaluateSquare : function(row, column) {
+        
         var self = this;
-
-        var selectedSquare = location.split('');
-        var selectedPiece = this.locations[location];
-
-        var row = selectedSquare[0];
-        var column = selectedSquare[1];
-
+        
         var vacantSquares = [];
         var occupiedSquares = [];
+        var strongerEnemyNearby = false;
+        var friendlyNearby = false;
+        
+        var square = this.locations[row][column];
 
-        var rowChar = row.charCodeAt(0);
-        var columnNum = parseInt(column);
+        if (square !== null) {
+            
+            //evaluateSquare one square to the left
+            var adjacentLeft = this.locations[row][column-1];
 
-        //Evaluate one square to the right
-        if(rowChar+1 <= (65 + this.rows)) {
+            if (adjacentLeft !== undefined) {
 
-            var rightLocation = String.fromCharCode(rowChar+1) + column;
+                if(adjacentLeft !== null && square.loyalty !== adjacentLeft.loyalty && square.rank > adjacentLeft.rank) {
+                    strongerEnemyNearby = true;
+                }
 
-            if(this.locations[rightLocation] === null) {
-                vacantSquares.push(rightLocation);
+                if(adjacentLeft !== null && square.loyalty === adjacentLeft.loyalty) {
+                    friendlyNearby = true;
+                }
+
+                if (adjacentLeft === null) {
+                    vacantSquares.push([row, column-1]);   
+                }
+                else {
+                    occupiedSquares.push([row, column-1]);   
+                }
             }
-            else {
-                occupiedSquares.push(rightLocation);
+
+            //evaluateSquare one square to the right
+            var adjacentRight = this.locations[row][column+1];
+
+            if (adjacentRight !== undefined) {
+
+                if(adjacentRight !== null && square.loyalty !== adjacentRight.loyalty && square.rank > adjacentRight.rank) {
+                    strongerEnemyNearby = true;
+                }
+
+                if(adjacentRight !== null && square.loyalty === adjacentRight.loyalty) {
+                    friendlyNearby = true;
+                }
+
+                if (adjacentRight === null) {
+                    vacantSquares.push([row, column+1]);   
+                }
+                else {
+                    occupiedSquares.push([row, column+1]);   
+                }
             }
+
+
+            var rowevaluateSquare = this.locations[row + 1];
+            if (rowevaluateSquare !== undefined) {
+
+                 //evaluateSquare one square down
+                var adjacentDown = this.locations[row + 1][column];
+
+
+
+                if (adjacentRight !== undefined) {
+
+                    if(adjacentDown !== null && square.loyalty !== adjacentDown.loyalty && square.rank > adjacentDown.rank) {
+                        strongerEnemyNearby = true;
+                    }
+
+                    if(adjacentDown !== null && square.loyalty === adjacentDown.loyalty) {
+                        friendlyNearby = true;
+                    }
+
+                    if (adjacentDown === null) {
+                        vacantSquares.push([row+1,column]);   
+                    }
+                    else {
+                        occupiedSquares.push([row+1,column]);   
+                    }
+                }
+            }
+
+            var rowevaluateSquare = this.locations[row - 1];
+            if (rowevaluateSquare !== undefined) {
+
+                 //evaluateSquare one square up
+                var adjacentUp = this.locations[row - 1][column];
+
+                if (adjacentUp !== undefined) {
+
+                    if(adjacentUp !== null && square.loyalty !== adjacentUp.loyalty && square.rank > adjacentUp.rank) {
+                        strongerEnemyNearby = true;
+                    }
+
+                    if(adjacentUp !== null && square.loyalty === adjacentUp.loyalty) {
+                        friendlyNearby = true;
+                    }
+
+                    if (adjacentUp === null) {
+                        vacantSquares.push([row-1,column]);   
+                    }
+                    else {
+                        occupiedSquares.push([row-1,column]);   
+                    }
+                }
+            }
+
+            square.vacantSquares = false && strongerEnemyNearby && !friendlyNearby ? [] : vacantSquares;
+            square.movablePieces = [];
+
+            if(!strongerEnemyNearby) {
+
+                //evaluateSquare each adjacent piece to determine whether it is a weaker enemy
+                occupiedSquares.forEach(function(occupiedSquare) {
+  
+                    var evaluateSquaredPiece = self.locations[occupiedSquare[0]][occupiedSquare[1]];
+
+                    if(square.loyalty !== evaluateSquaredPiece.loyalty && square.rank < evaluateSquaredPiece.rank) {
+                        square.movablePieces.push([occupiedSquare[0],occupiedSquare[1]]);
+                    }
+                });
+            }
+            
         }
-
-        //Evaluate one square to the left
-        if(rowChar-1 >= 65) {
-
-            var leftLocation = String.fromCharCode(rowChar-1) + column;
-
-            if(this.locations[leftLocation] === null) {
-                vacantSquares.push(leftLocation);
-            }
-            else {
-                occupiedSquares.push(leftLocation);
-            }
-        }
-
-        //Evaluate one square up
-        if(columnNum+1 <= this.columns) {
-
-            var upLocation = row + (columnNum+1);
-
-            if(this.locations[upLocation] === null) {
-                vacantSquares.push(upLocation);
-            }
-            else {
-                occupiedSquares.push(upLocation);
-            }
-
-        }
-
-        //Evaluate one square down
-        if(columnNum-1 >= 1) {
-
-            var downLocation = row + (columnNum-1);
-
-            if(this.locations[downLocation] === null) {
-                vacantSquares.push(downLocation);
-            }
-            else {
-                occupiedSquares.push(downLocation);
-            }
-
-        }
-
-        this.locations[location].vacantSquares = vacantSquares;
-
-        //Evaluate each adjacent piece to determine whether it is a weaker enemny
-        occupiedSquares.forEach(function(occupiedSquare) {
-
-            var evaluatedPiece = self.locations[occupiedSquare];
-
-            if(selectedPiece.loyalty !== evaluatedPiece.loyalty && selectedPiece.rank < evaluatedPiece.rank) {
-                selectedPiece.weakerEnemies.push(occupiedSquare);
-            }
-
-        });
-
+        
     },
-
-    rows : null,
-    columns : null,
-    locations : [],
 
 };
